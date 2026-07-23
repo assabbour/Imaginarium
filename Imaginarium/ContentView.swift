9ab66c5d9f8d145b1ebe7e564d7f8f6a554898b4
Yamine3 <yamine.aissani.pro@gmail.com>
2026-07-23 16:07:16 +0200

    First commit

---
+++ Imaginarium/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Imaginarium
+//
+//  Created by Apprenant 109 on 23/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Imaginarium/ImaginariumApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImaginariumApp.swift
+//  Imaginarium
+//
+//  Created by Apprenant 109 on 23/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct ImaginariumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
