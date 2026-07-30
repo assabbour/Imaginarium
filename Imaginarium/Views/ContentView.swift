d6c9771dbe270bd7e115fdf45298b2b4c53450f5
Filip <filiprizov@gmail.com>
2026-07-30 14:09:33 +0200

    fixed colors

--- Imaginarium/Views/ContentView.swift
+++ Imaginarium/Views/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             BackgroundGradient()
+            //LALA
             VStack {
                 
                 Image(systemName: "globe")
