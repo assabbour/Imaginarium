1de5340f748e14a04312848e346162a540b71d9c
Filip <filiprizov@gmail.com>
2026-08-06 16:20:03 +0200

    MapAnnotation View done.

--- Imaginarium/Components/MapAnnotationView.swift
+++ Imaginarium/Components/MapAnnotationView.swift
@@ -8,11 +8,31 @@
 import SwiftUI
 
 struct MapAnnotationView: View {
+    let wiki: Wiki
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .center) {
+            BackgroundGradient()
+            UnevenRoundedRectangle(topLeadingRadius: 35.0,bottomLeadingRadius: 25.0, bottomTrailingRadius: 25.0, topTrailingRadius: 35.0)
+                .frame(width: 95, height: 92)
+                .foregroundStyle(.backgroundDarkBlue)
+            Triangle()
+                .rotation(Angle(degrees: 180))
+                .fill(.backgroundDarkBlue)
+                .frame(width: 70, height: 15)
+                .offset(x:0, y:50)
+            AsyncImage(url: wiki.imageName) { image in
+                image.resizable()
+                } placeholder: {
+                    Image(systemName: "photo")
+                }
+                    .scaledToFill()
+                    .frame(width: 84, height: 84)
+                    .clipped()
+                    .cornerRadius(28)
+                }
     }
 }
 
 #Preview {
-    MapAnnotationView()
+    MapAnnotationView(wiki: MockData.wikis[0])
 }
