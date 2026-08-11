fb9be3bfd361334f8092d6bb7be72d88a07f7125
Melissa <meli.tournerie@gmail.com>
2026-08-12 00:23:58 +0200

    add TopicFormView et TopicCommentFormView

--- Imaginarium/Models/Topic.swift
+++ Imaginarium/Models/Topic.swift
@@ -13,14 +13,14 @@ struct Topic:Hashable, Identifiable {
     let id = UUID()
     /// Sujet de la discussion.
     var subject: String
-    let title: String
+    var title: String
     var creator: User
-    let message: String?
+    var message: String?
     var category : TopicCategory
     /// Image facultative associée à la publication.
-    let image: URL?
+    var image: URL?
     /// Réponses des autres utilisateurs.
-    let comments: [Comment]
+    var comments: [Comment]
 }
 
 
