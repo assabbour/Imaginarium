ffec4dea2ef2e5a8f88a9cd52f81ce0114dfafdf
assabbour <assabbour@hotmail.com>
2026-08-12 10:29:01 +0200

    Correction des bugs liés à l'affichage du profil utilisateur

--- Imaginarium/Views/Profile/UserDescriptionView.swift
+++ Imaginarium/Views/Profile/UserDescriptionView.swift
@@ -1,8 +1,24 @@
-//
-//  UserDescriptionView.swift
-//  Imaginarium
-//
-//  Created by apprenant 108 on 12/08/2026.
-//
+import SwiftUI
 
-import Foundation
+struct UserDescriptionView: View {
+    let description: String
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text("Description :")
+                .font(.headline)
+                .foregroundColor(.white)
+            Rectangle()
+                .frame(height: 1)
+                .foregroundColor(.cyan.opacity(0.6))
+
+            Text(description)
+                .font(.body)
+                .foregroundColor(.white.opacity(0.9))
+                .fixedSize(horizontal: false, vertical: true)
+        }
+        .padding()
+        .background(Color.black.opacity(0.2))
+        .cornerRadius(12)
+    }
+}
