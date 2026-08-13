32294df5c42a319c93e1e6789c7023f2f164082c
assabbour <assabbour@hotmail.com>
2026-08-13 15:08:33 +0200

    Amélioration du design du bouton d'ajout sur le profil

--- Imaginarium/Views/Profile/UserHeaderView.swift
+++ Imaginarium/Views/Profile/UserHeaderView.swift
@@ -47,8 +47,10 @@ struct UserHeaderView: View {
                 Spacer()
                 Button(action: onEditTapped) {
                     HStack(spacing: 5) {
-                        Text("Edit")
-                        Image(systemName: "pencil")
+                        Image(systemName: "plus")
+                                .font(.system(size: 18, weight: .bold))
+                                .padding(10)
+                                .clipShape(Circle())
                     }
                     .font(.subheadline)
                     .fontWeight(.medium)
